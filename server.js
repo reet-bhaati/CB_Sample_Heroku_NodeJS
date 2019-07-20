d26409a1b53971d9fe443fc088f0271ab8b03706
reet-bhaati <52093948+reet-bhaati@users.noreply.github.com>
2019-07-20 19:50:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 4444
 
 app.get('/', (req, res) => {
-  console.log('Some visitor came by')
+  console.log('new visitor')
   res.send('Hello World')
 })
 
